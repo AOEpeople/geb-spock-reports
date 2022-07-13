0f0ebcaf25cf6de2478f1c79df6ddb7f630f3b12
JoroMM101 <joro.mm.101@gmail.com>
2022-07-14 02:29:38 +0300

    Use WebDriverManager instead of chromedriver-updater

--- sample/src/test/resources/GebConfig.groovy
+++ sample/src/test/resources/GebConfig.groovy
@@ -1,7 +1,20 @@
 import com.aoe.gebspockreports.GebReportingListener
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.openqa.selenium.Dimension
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 
 reportingListener = new GebReportingListener()
 reportsDir = 'build/geb-spock-reports'
 
-driver = { new ChromeDriver() }
+driver = {
+    WebDriverManager.chromedriver().setup()
+    ChromeOptions chromeOptions = new ChromeOptions()
+    chromeOptions.setHeadless(true)
+
+    Dimension dimension = new Dimension(1920, 1080)
+    ChromeDriver chromeDriver = new ChromeDriver(chromeOptions)
+    chromeDriver.manage().window().setSize(dimension)
+
+    return chromeDriver
+}
